e242de37ac0e83ef259fe6abd78bc9da6c15ce60
yfan25 <yfan25@byu.edu>
2026-07-21 17:39:00 -0600

    phase 4: optimized code quality

--- server/src/main/java/server/ServerMain.java
+++ server/src/main/java/server/ServerMain.java
@@ -8,15 +8,3 @@ public class ServerMain {
         System.out.println("♕ 240 Chess Server");
     }
 }
-
-
-//package server;
-//
-//import chess.*;
-//
-//public class ServerMain {
-//    public static void main(String[] args) {
-//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-//        System.out.println("♕ 240 Chess Server: " + piece);
-//    }
-//}
