4b3da2c9b8d1af96bf20b5c1571b4ea5b1ac49de
yfan25 <yfan25@byu.edu>
2026-07-29 10:30:05 -0600

    phase 5: modified client main

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,10 +1,14 @@
 package client;
 
 import chess.*;
+import ui.Repl;
 
 public class ClientMain {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        ServerFacade server = new ServerFacade(8080);
+        ChessClient client = new ChessClient(server);
+        Repl repl = new Repl(client);
+
+        repl.run();
     }
 }
