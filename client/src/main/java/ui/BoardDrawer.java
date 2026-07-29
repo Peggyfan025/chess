5597852b3aded85fde10d0d4ce63957973f10a1d
yfan25 <yfan25@byu.edu>
2026-07-29 00:55:31 -0600

    phase 5: added row logic and setting square color

--- client/src/main/java/ui/BoardDrawer.java
+++ client/src/main/java/ui/BoardDrawer.java
@@ -3,6 +3,7 @@ package ui;
 import chess.ChessBoard;
 import chess.ChessGame;
 import chess.ChessPiece;
+import chess.ChessPosition;
 
 import static ui.EscapeSequences.*;
 
@@ -12,15 +13,52 @@ public class BoardDrawer {
         ChessBoard board = new ChessBoard();
         board.resetBoard();
 
+        StringBuilder result = new StringBuilder();
+        appendColumnLabels(result, perspective);
+
         for (int index = 0; index < 8; index++) {
             int row;
             if (perspective == ChessGame.TeamColor.WHITE) {
                 row = 8 - index;
-            } else {
+            }
+            else {
                 row = index + 1;
             }
-
+            appendRow(result, board, row, perspective);
         }
+        appendColumnLabels(result, perspective);
+        result.append(RESET_BG_COLOR);
+        result.append(RESET_TEXT_COLOR);
+
+        return result.toString();
+    }
+
+    private static void appendRow(StringBuilder result, ChessBoard board, int row, ChessGame.TeamColor perspective) {
+
+        result.append(RESET_BG_COLOR);
+        result.append(SET_TEXT_COLOR_WHITE);
+        result.append(" ").append(row).append(" ");
+
+        for (int index = 0; index < 8; index++) {
+            int column;
+
+            if (perspective == ChessGame.TeamColor.WHITE) {
+                column = index + 1;
+            }
+            else {
+                column = 8 - index;
+            }
+
+            setSquareColor(result, row, column);
+
+            ChessPosition position = new ChessPosition(row, column);
+            ChessPiece piece = board.getPiece(position);
+            result.append(getPieceSymbol(piece));
+        }
+
+        result.append(RESET_BG_COLOR);
+        result.append(SET_TEXT_COLOR_WHITE);
+        result.append(" ").append(row).append("\n");
     }
 
     private static void appendColumnLabels(
@@ -49,6 +87,22 @@ public class BoardDrawer {
         }
         result.append("\n");
     }
+
+    private static void setSquareColor(
+            StringBuilder result,
+            int row,
+            int column) {
+
+        // a1 is a dark square.
+        if ((row + column) % 2 == 0) {
+            result.append(SET_BG_COLOR_DARK_GREY);
+            result.append(SET_TEXT_COLOR_WHITE);
+        } else {
+            result.append(SET_BG_COLOR_LIGHT_GREY);
+            result.append(SET_TEXT_COLOR_BLACK);
+        }
+    }
+
     private static String getPieceSymbol(ChessPiece piece) {
         if (piece == null) {
             return EMPTY;
