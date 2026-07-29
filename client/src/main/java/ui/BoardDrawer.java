7401750b550971312ece81d1a8e995811bb7e2f4
yfan25 <yfan25@byu.edu>
2026-07-29 00:45:42 -0600

    phase 5: added column logic and piece logic

--- client/src/main/java/ui/BoardDrawer.java
+++ client/src/main/java/ui/BoardDrawer.java
@@ -2,9 +2,9 @@ package ui;
 
 import chess.ChessBoard;
 import chess.ChessGame;
+import chess.ChessPiece;
 
-import static ui.EscapeSequences.RESET_BG_COLOR;
-import static ui.EscapeSequences.RESET_TEXT_COLOR;
+import static ui.EscapeSequences.*;
 
 
 public class BoardDrawer {
@@ -21,4 +21,57 @@ public class BoardDrawer {
             }
 
         }
+    }
+
+    private static void appendColumnLabels(
+            StringBuilder result,
+            ChessGame.TeamColor perspective) {
+
+        result.append(RESET_BG_COLOR);
+        result.append(SET_TEXT_COLOR_WHITE);
+        result.append("   ");
+
+        for (int index = 0; index < 8; index++) {
+            int column;
+
+            if (perspective == ChessGame.TeamColor.WHITE) {
+                column = index + 1;
+            }
+            else {
+                column = 8 - index;
+            }
+
+            char letter = (char) ('a' + column - 1);
+
+            result.append(" ")
+                    .append(letter)
+                    .append(" ");
+        }
+        result.append("\n");
+    }
+    private static String getPieceSymbol(ChessPiece piece) {
+        if (piece == null) {
+            return EMPTY;
+        }
+
+        if (piece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+            return switch (piece.getPieceType()) {
+                case KING -> WHITE_KING;
+                case QUEEN -> WHITE_QUEEN;
+                case BISHOP -> WHITE_BISHOP;
+                case KNIGHT -> WHITE_KNIGHT;
+                case ROOK -> WHITE_ROOK;
+                case PAWN -> WHITE_PAWN;
+            };
+        }
+
+        return switch (piece.getPieceType()) {
+            case KING -> BLACK_KING;
+            case QUEEN -> BLACK_QUEEN;
+            case BISHOP -> BLACK_BISHOP;
+            case KNIGHT -> BLACK_KNIGHT;
+            case ROOK -> BLACK_ROOK;
+            case PAWN -> BLACK_PAWN;
+        };
+    }
 }
